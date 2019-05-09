6750596a9fc36f072bd87fa7d2b82c4bdd46cf0d
dat <datthangvan@gmail.com>
2019-05-10 06:13:41 +0700

    update Deep learning

--- DeepLearning/example04.py
+++ DeepLearning/example04.py
@@ -14,3 +14,16 @@ LOG_DIR = "./tmp"
 if os.path.isdir(LOG_DIR) == False:
     os.mkdir(LOG_DIR)
 
+
+def add_variables_summary(tf_variable, summary_name):
+    with tf.name_scope(summary_name + "_summary"):
+        mean = tf.reduce_mean(tf_variable)
+        tf.summary.scalar("Mean", mean)
+        with tf.name_scope("standard_deviation"):
+            standard_deviation = tf.sqrt(tf.reduce_mean(tf.square(tf_variable - mean)))
+
+    tf.summary.scalar("standarDeviation", standard_deviation)
+    tf.summary.scalar("Maximum", tf.reduce_max(tf_variable))
+    tf.summary.scalar("Minimum", tf.reduce_min(tf_variable))
+    tf.summary.histogram("Histogram", tf_variable)
+
