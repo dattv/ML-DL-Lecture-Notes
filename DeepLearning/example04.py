a4ec147902af4e3b320be552f1c7f1c593119cc3
dat <datthangvan@gmail.com>
2019-05-10 06:08:47 +0700

    update Deep learning

--- DeepLearning/example04.py
+++ DeepLearning/example04.py
@@ -1,3 +1,16 @@
 import tensorflow as tf
 import os
 
+# Load mnist dataset
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist_data = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+train_mnist_data = mnist_data.train
+test_mnist_data = mnist_data.test
+valid_mnist_data = mnist_data.validation
+
+LOG_DIR = "./tmp"
+if os.path.isdir(LOG_DIR) == False:
+    os.mkdir(LOG_DIR)
+
