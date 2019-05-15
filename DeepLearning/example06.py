be6d2e688520a2940f73fdc203235f54c4f2f508
dat <datthangvan@gmail.com>
2019-05-15 10:54:45 +0700

    update Deep learning

--- DeepLearning/example06.py
+++ DeepLearning/example06.py
@@ -8,6 +8,8 @@ import tarfile
 import zipfile
 import numpy as np
 
+import _pickle
+
 import time
 from tqdm import tqdm
 
@@ -56,3 +58,46 @@ if os.path.isfile(full_file_path) == False:
 tar = tarfile.open(full_file_path)
 tar.extractall()
 tar.close()
+
+# Process data
+DATA_PATH = "./cifar-10-batches-py"
+
+
+def unpickle(file):
+    with open(os.path.join(DATA_PATH, file), 'rb') as fo:
+        dict = cPickle.load(fo)
+
+    return dict
+
+
+def one_hot(vec, vals=10):
+    n = len(vec)
+    out = np.zeros((n, vals))
+
+    out[range(n), vec] = 1
+    return out
+
+
+class CifarLoader(object):
+
+    def __init__(self, source_files):
+        self._source = source_files
+        self._i = 0
+        self.images = None
+        self.labels = None
+
+    def load(self):
+        data = [unpickle().unpack(f) for f in self._source]
+        images = np.vstack([d["data"] for d in data])
+        n = len(images)
+
+        self.images = images.reshape(n, 3, 32, 32).transpose(0, 2, 3, 1).astype(float) / 255.
+        self.labels = one_hot(np.hstack([d["labels"] for d in data]), 10)
+
+        return self
+
+    def nex_batch(self, batch_size):
+        x, y = self.images[self._i:self._i + batch_size], self.labels[self._i:self._i + batch_size]
+        self._i = (self._i + batch_size) % len(self.images)
+
+        return x, y
