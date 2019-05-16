a508825e12447416764a32b0e4f60ff656861acf
dat <datthangvan@gmail.com>
2019-05-16 14:25:12 +0700

    update Deep learning

--- DeepLearning/example07.py
+++ DeepLearning/example07.py
@@ -1,2 +1,127 @@
-import tensorflow as tf
+import tarfile
+import zipfile
 import numpy as np
+
+import pickle as cPickle
+
+import time
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+CIFAR_DIR = "./CIFA"
+
+
+def my_hook(t):
+    """
+    Wraps tqdm instance
+    :param t:
+    :return:
+    """
+    last_b = [0]
+
+    def inner(b=1, bsize=1, tsize=None):
+        """
+
+        :param b:       int option
+        :param bsize:   int
+        :param tsize:
+        :return:
+        """
+        if tsize is not None:
+            t.total = tsize
+        t.update((b - last_b[0]) * bsize)
+        last_b[0] = b
+
+    return inner
+
+
+if os.path.isdir(CIFAR_DIR) == False:
+    os.mkdir(CIFAR_DIR)
+
+cifar_usr = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+name = cifar_usr.split("/")
+name = name[len(name) - 1]
+
+full_file_path = CIFAR_DIR + "/" + name
+if os.path.isfile(full_file_path) == False:
+    print("downloading from: {}".format(cifar_usr))
+    with tqdm(unit="B", unit_scale=True, leave=True, miniters=1, desc=cifar_usr.split("/")[-1]) as t:
+        urllib.request.urlretrieve(cifar_usr, filename=full_file_path, reporthook=my_hook(t), data=None)
+    print("finish download")
+
+# extract compressed file
+tar = tarfile.open(full_file_path)
+tar.extractall()
+tar.close()
+
+# Process data
+DATA_PATH = "./cifar-10-batches-py"
+
+
+def unpickle(file):
+    with open(os.path.join(DATA_PATH, file), 'rb') as fo:
+        dict = cPickle.load(fo, encoding='bytes')
+
+    return dict
+
+
+def one_hot(vec, vals=10):
+    n = len(vec)
+    out = np.zeros((n, vals))
+
+    out[range(n), vec] = 1
+    return out
+
+
+class CifarLoader(object):
+
+    def __init__(self, source_files):
+        self._source = source_files
+        self._i = 0
+        self.images = None
+        self.labels = None
+
+    def load(self):
+        data = [unpickle(f) for f in self._source]
+        images = np.vstack([d[b"data"] for d in data])
+        n = len(images)
+
+        self.images = images.reshape(n, 3, 32, 32).transpose(0, 2, 3, 1).astype(float) / 255.
+        self.labels = one_hot(np.hstack([d[b"labels"] for d in data]), 10)
+
+        return self
+
+    def nex_batch(self, batch_size):
+        x, y = self.images[self._i:self._i + batch_size], self.labels[self._i:self._i + batch_size]
+        self._i = (self._i + batch_size) % len(self.images)
+
+        return x, y
+
+
+class CifarDataManager(object):
+
+    def __init__(self):
+        self.train = CifarLoader(["data_batch_{}".format(i) for i in range(1, 6)]).load()
+        self.test = CifarLoader(["test_batch"]).load()
+
+
+def display_cifar(images, size):
+    n = len(images)
+    plt.figure()
+    plt.gca().set_axis_off()
+
+    im = np.vstack([np.hstack([images[np.random.choice(n)] for i in range(size)]) for i in range(size)])
+
+    plt.imshow(im)
+    plt.savefig("./CIFA/cifar-10-" + str(n))
+    plt.show()
+
+
+d = CifarDataManager()
+train_images = d.train.images
+train_labels = d.train.labels
+
+test_images = d.test.images
+test_labels = d.test.labels
+
+# display_cifar(images, 10)
