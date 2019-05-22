ee23e5a2014549fd9db462ac5e3deef0d63c78be
dat <datthangvan@gmail.com>
2019-05-22 09:19:55 +0700

    update deeplearning

--- DeepLearning/example08.py
+++ DeepLearning/example08.py
@@ -53,3 +53,44 @@ with tf.Session() as session:
                                                      signature_def_map={'prediction': signature_def},
                                                      legacy_init_op=tf.group(tf.tables_initializer(), name='legacy_init_op'))
     saved_model_builder.save()
+
+
+# Serning the trained model
+
+from grpc.beta import implementations
+import numpy
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+
+concurrency = 1
+num_tests= 100
+host = ""
+port = 8000
+work_dir = "./tmp"
+
+def _create_rpc_callback():
+    def _callback(result):
+        response = np.array(result.result().outputs['y'].float_val)
+        prediction = np.argmax(response)
+
+        print(prediction)
+
+    return _callback
+
+test_data_set = mnist.test
+test_image = mnist.test.images[0]
+
+predict_request = predict_pb2.PredictRequest()
+predict_request.model_spec.name = 'mnist'
+predict_request.model_spec.signature_name = 'prediction'
+
+predict_channel = implementations.insecure_channel(host, int(port))
+predict_stub = prediction_service_pb2.beta_create_PredictionService_stub(predict_channel)
+
+predict_request.inputs['x'].CopyFrom(
+    tf.contrib.util.make_tensor_proto(test_image, shape=[1, test_image.size]))
+result = predict_stub.Predict.future(predict_request, 3.0)
+result.add_done_callback(
+    _create_rpc_callback())
