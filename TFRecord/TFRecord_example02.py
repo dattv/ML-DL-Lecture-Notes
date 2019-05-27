0de0beaefb655f194573d370a84886f96a711e5d
dat <datthangvan@gmail.com>
2019-05-28 00:38:50 +0700

    update Deeplearning

--- TFRecord/TFRecord_example02.py
+++ TFRecord/TFRecord_example02.py
@@ -1,11 +1,27 @@
-# I convert the example in https://github.com/Hvass-Labs/TensorFlow-Tutorials/blob/master/18_TFRecords_Dataset_API.ipynb
-# to python
-
-import matplotlib.pyplot as plt
-from matplotlib.image import imread
-import tensorflow as tf
-import numpy as np
-import sys
+from __future__ import print_function
 import os
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets import mnist
+import numpy as np
 
-print(tf.__version__)
+work_dir = os.getcwd()
+mnist_dir = os.path.join(work_dir, "mnist")
+
+if os.path.isdir(mnist_dir) == False:
+    os.mkdir(mnist_dir)
+
+save_dir = mnist_dir
+datasets = mnist.read_data_sets(save_dir,
+                               dtype=tf.uint8,
+                               reshape=False,
+                               validation_size=1000)
+
+data_splits = ["train", "test", "validation"]
+
+for d in range(len(data_splits)):
+    print("saving: {}".format(data_splits[d]))
+    data_set = datasets[d]
+
+    file_name = os.path.join(save_dir, data_splits[d] + ".tfrecords")
+
+
