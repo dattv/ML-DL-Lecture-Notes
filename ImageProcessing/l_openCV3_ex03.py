82b7228ed025b2682e8c81e37c799341ee298647
dat <datthangvan@gmail.com>
2019-06-15 00:56:05 +0700

    update imageProcessing

--- ImageProcessing/l_openCV3_ex03.py
+++ ImageProcessing/l_openCV3_ex03.py
@@ -2,4 +2,38 @@ import cv2 as cv
 import numpy as np
 import os
 
+root_path = os.path.dirname(os.path.dirname(__file__))
+sample_img_file_path = os.path.join(root_path, "DeepLearning")
+sample_img_file_path = os.path.join(sample_img_file_path, "kagglecatsanddogs_3367a")
+sample_img_file_path = os.path.join(sample_img_file_path, "PetImages")
+sample_img_file_path = os.path.join(sample_img_file_path, "Cat")
+sample_img_file_path = os.path.join(sample_img_file_path, "1.jpg")
+
+image = cv.imread(sample_img_file_path)
+image[0, 0] = [255, 255, 255]
+cv.imshow("image", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+print(image.item(20, 20, 0))
+image.itemset((20, 20, 0), 12)
+print(image.item(20, 20, 0))
+
+# setting all green value to 0
+image[:, :, 1] = 0
+cv.imshow("green", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# copy and roi
+my_roi = image[0:100, 0:100]
+image[100:200, 100:200] = my_roi
+cv.imshow("roi", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+print(image.shape)
+print(image.size)
+print(image.dtype)
 
