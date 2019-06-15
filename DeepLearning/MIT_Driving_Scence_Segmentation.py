d2aaeaf53c273b2c267fd091fa6ba08deb3f4510
dattv <datthangvan@gmail.com>
2019-06-15 09:31:59 +0700

    update segmentation

--- DeepLearning/MIT_Driving_Scence_Segmentation.py
+++ DeepLearning/MIT_Driving_Scence_Segmentation.py
@@ -206,3 +206,53 @@ def run_visualization(SAMPLE_IMAGE):
     vis_segmentation(original_im, seg_map)
 
 run_visualization(SAMPLE_IMAGE)
+
+
+# Run on the same video
+def vis_segmentation_stream(image, seg_map, index):
+    """
+    Visualizes segmentation over lay view and stream it with IPython display.
+    :param image:
+    :param seg_map:
+    :param index:
+    :return:
+    """
+    plt.figure(figsize=(12, 7))
+
+    seg_image = label_to_color_image(seg_map).astype(np.uint8)
+    plt.imshow(image)
+    plt.imshow(seg_image, alpha=0.7)
+    plt.axis("off")
+    plt.title("segmentation overlay | fram #{}".format(index))
+    plt.grid("off")
+    plt.tight_layout()
+
+    plt.close()
+
+def run_visualization_video(frame, index):
+    """Inferences Deeplap model on a video file and stream the visualization."""
+    original_im = Image.fromarray((frame[..., ::-1]))
+    seg_map = MODEL.run(original_im)
+    vis_segmentation_stream(original_im, seg_map, index)
+
+SAMPLE_VIDEO = 'mit_driveseg_sample.mp4'
+if not os.path.isfile(SAMPLE_VIDEO):
+    print('downloading the sample video...')
+    SAMPLE_VIDEO = urllib.request.urlretrieve('https://github.com/lexfridman/mit-deep-learning/raw/master/tutorial_driving_scene_segmentation/mit_driveseg_sample.mp4')[0]
+print('running deeplab on the sample video...')
+
+video = cv.VideoCapture(SAMPLE_VIDEO)
+# num_frames = 598  # uncomment to use the full sample video
+num_frames = 30
+
+try:
+    for i in range(num_frames):
+        _, frame = video.read()
+        if not _: break
+        run_visualization_video(frame, i)
+        # IPython.display.clear_output(wait=True)
+except KeyboardInterrupt:
+    plt.close()
+    print("Stream stopped.")
+
+
--- ImageProcessing/l_openCV3_ex04.py
+++ ImageProcessing/l_openCV3_ex04.py
@@ -2,6 +2,8 @@
 import numpy as np
 import cv2 as cv
 import os
+
+from numpy.ma.extras import _fromnxfunction_allargs
 from tqdm import tqdm
 import urllib.request
 
@@ -16,7 +18,6 @@ def my_hook(t):
 
     def inner(b=1, bsize=1, tsize=None):
         """
-
         :param b:       int option
         :param bsize:   int
         :param tsize:
@@ -31,11 +32,48 @@ def my_hook(t):
 
 
 # download video
-video_url = "http://www.ee.cuhk.edu.hk/~xgwang/filtTrk_parkinglot.mat"
+video_url = "http://www.cvc.uab.es/~bagdanov/master/videos/car-overhead-1.avi"
 PATH = os.getcwd()
 video_folder = os.path.join(PATH, "video")
 if os.path.isdir(video_folder) == False:
     os.mkdir(video_folder)
 
     with tqdm(unit="B", unit_scale=True, leave=True, miniters=1, desc=video_folder.split("/")[-1]) as t:
-        urllib.request.urlretrieve(video_url, filename=os.path.join(video_folder, "video01"), reporthook=my_hook(t), data=None)
+        urllib.request.urlretrieve(video_url, filename=os.path.join(video_folder, "video01.avi"), reporthook=my_hook(t), data=None)
+
+video_capture = cv.VideoCapture(os.path.join(video_folder, "video01.avi"))
+fps = video_capture.get(cv.CAP_PROP_FPS)
+size = (int(video_capture.get(cv.CAP_PROP_FRAME_WIDTH)),
+        int(video_capture.get(cv.CAP_PROP_FRAME_HEIGHT)))
+
+video_writer = cv.VideoWriter("myout.avi",
+                              cv.VideoWriter_fourcc("I", "4", "2", "0"), fps, size)
+
+success, frame = video_capture.read()
+while success:
+    video_writer.write(frame)
+    success, frame = video_capture.read()
+
+video_capture.release()
+
+
+clicked = False
+def onMouse(event, x, y, flags, param):
+    global  clicked
+    if event == cv.EVENT_LBUTTONUP:
+        clicked = True
+
+video_capture = cv.VideoCapture(os.path.join(video_folder, "video01.avi"))
+cv.namedWindow("my_window")
+cv.setMouseCallback("my_window", onMouse)
+success, frame = video_capture.read()
+
+while success and cv.waitKey(1) == -1 and not clicked:
+    cv.imshow("my_window", frame)
+    success, frame = video_capture.read()
+
+cv.destroyAllWindows("my_window")
+video_capture.release()
+
+
+
