30d5a7cec7b097838aa4ec93b106065b1dbc8fd1
dat <datthangvan@gmail.com>
2019-06-15 14:53:23 +0700

    update siamese

--- siamese_tf/Omniglot_dataset.py
+++ siamese_tf/Omniglot_dataset.py
@@ -31,7 +31,7 @@ root_path = os.path.dirname(os.path.dirname(__file__))
 siamese_folder = os.path.join(root_path, "siamese_tf")
 
 image_background_url = "https://github.com/brendenlake/omniglot/raw/master/python/images_background.zip"
-images_eveluation_url = "https://github.com/brendenlake/omniglot/raw/master/python/images_evaluation.zip"
+images_evaluation_url = "https://github.com/brendenlake/omniglot/raw/master/python/images_evaluation.zip"
 
 omniglot_folder = os.path.join(siamese_folder, "omniglot")
 if os.path.isdir(omniglot_folder) == False:
@@ -43,3 +43,15 @@ image_background_full_file_path = os.path.join(omniglot_folder, image_back_groun
 with tqdm(unit="B", unit_scale=True, leave=True, miniters=1, desc=image_background_url.split("/")[-1]) as t:
     urllib.request.urlretrieve(image_background_url, filename=image_background_full_file_path, reporthook=my_hook(t), data=None)
 
+zip_ref = zipfile.ZipFile(image_background_full_file_path, "r")
+zip_ref.extractall(omniglot_folder)
+zip_ref.close()
+
+images_evaluation_file_name = os.path.split(images_evaluation_url)[1]
+images_evalutaion_full_file_path = os.path.join(omniglot_folder, images_evaluation_file_name)
+with tqdm(unit="B", unit_scale=True, leave=True, miniters=1, desc=images_evaluation_url.split("/")[-1]) as t:
+    urllib.request.urlretrieve(images_evaluation_url, filename=images_evalutaion_full_file_path, reporthook=my_hook(t), data=None)
+
+zip_ref = zipfile.ZipFile(images_evalutaion_full_file_path, "r")
+zip_ref.extractall(omniglot_folder)
+zip_ref.close()
