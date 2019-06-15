1006515db4e0a267e3d9c73885129836c24c3b58
dat <datthangvan@gmail.com>
2019-06-15 15:15:12 +0700

    update siamese

--- siamese_tf/Omniglot_loader.py
+++ siamese_tf/Omniglot_loader.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import tensorflow as tf
 
@@ -15,3 +17,26 @@ def omniglot_loader(path, n=0):
     curr_y = n
 
     # we load every alphabet seperately so we can isolate them later
+    for alphabet in os.listdir(path):
+        print("loading alphabet: {}".format(alphabet))
+        lang_dict[alphabet] = [curr_y, None]
+        alphabet_path = os.path.join(path, alphabet)
+
+        # every letter/category has it's own column in the array, so load seperately
+
+
+
+def main():
+    root_path = os.path.dirname(os.path.dirname(__file__))
+    siamese_dir = os.path.join(root_path, "siamese_tf")
+    omniglot_dir = os.path.join(siamese_dir, "omniglot")
+
+    train_folder = os.path.join(omniglot_dir, "images_background")
+
+    omniglot_loader(train_folder, 10)
+
+
+if __name__ == '__main__':
+    main()
+
+
