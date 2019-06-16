03bb637e6cf7eb9aba1a09008f81f4f6c63621d9
dattv <datthangvan@gmail.com>
2019-06-17 02:50:53 +0700

    update siameseNN

--- siamese_tf/siamese_model.py
+++ siamese_tf/siamese_model.py
@@ -2,18 +2,28 @@ import tensorflow as tf
 
 
 class siamese():
-    def __init__(self, input_shape):
-        print("init")
+    def __init__(self):
+        self.stddev_ = 0.1
 
-    def model(self, input_tensor1, input_tensor2):
+    def make_model(self, input_tensor1, input_tensor2):
         encoded_l = self.sub_model(input_tensor1)
         encoded_r = self.sub_model(input_tensor2)
 
+        l1_distance = tf.abs(encoded_l - encoded_r, name="L1_DISTANCE")
 
+        with tf.name_scope("fully_layer_2") as scope:
+            with tf.name_scope("weights") as scope:
+                w_flat2_1 = tf.Variable(tf.truncated_normal([4096, 1], stddev=self.stddev_), name="w_flat2_1")
+            with tf.name_scope("biases") as scope:
+                b_flat2_1 = tf.Variable(tf.constant(0.1, shape=[1]), name="b_flat2_1")
+
+            pred = tf.nn.sigmoid(tf.matmul(l1_distance, w_flat2_1) + b_flat2_1, name="pred")
+
+        return pred
 
     def sub_model(self, input_tensor):
         n_chanel = input_tensor.shape[3]
-        stddev_ = 0.1
+        stddev_ = self.stddev_
 
         with tf.name_scope("conv_layer_1") as scope:
             with tf.name_scope("weights") as scope:
@@ -103,3 +113,12 @@ class siamese():
             fully1_1 = tf.nn.sigmoid(fully1_1, name="FULLY1_1")
 
         return fully1_1
+
+def main():
+    model = siamese()
+
+    img1 = tf.placeholder()
+
+
+if __name__ == '__main__':
+    main()
