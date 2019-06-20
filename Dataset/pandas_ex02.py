487e43e08c489c5826aa0de347226baa49afbfb2
dattv <datthangvan@gmail.com>
2019-06-20 11:58:58 +0700

    update Pandas

---
+++ Dataset/pandas_ex02.py
@@ -0,0 +1,15 @@
+import os
+
+import numpy as np
+import pandas
+
+my_dict = {"name":["a", "b", "c", "d", "e", "f", "g"],
+           "age":[20, 27, 35, 55, 18, 21, 35],
+           "designation": ["VP", "CEO", "CFO", "VP", "VP", "CEO", "MD"]}
+
+cf = pandas.DataFrame(my_dict)
+root_path = os.path.dirname(os.path.dirname(__file__))
+
+data_set_path = os.path.join(root_path, "Dataset")
+full_file_path = os.path.join(data_set_path, "pandas_ex02.csv")
+cf.to_csv(full_file_path)
