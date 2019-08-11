617c85e481ca80fdd3b19b7d7c763b1d4afec9df
dat <datthangvan@gmail.com>
2019-08-11 11:37:35 +0700

    update update cifar packages

--- Dataset/cifar/cifar_dataset.py
+++ Dataset/cifar/cifar_dataset.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-import tqdm
-from urllib.request import urlretrieve
+from Dataset.download.download import download
+import tarfile
 
 
 class cifar_10:
@@ -14,8 +14,32 @@ class cifar_10:
         else:
             self._save_folder = save_folder
 
-        
+        full_file_path = os.path.join(save_folder, os.path.split(self._url)[1])
+        list_file = os.listdir(self._save_folder)
+        if full_file_path not in list_file:
+            download(self._url, self._save_folder)
 
+        tar = tarfile.open(full_file_path)
+        tar.extractall()
+        tar.close()
+
+class cifar_100:
+    def __init__(self, url="https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz", save_folder=None):
+        self._url = url
+
+        if save_folder == None:
+            self._save_folder = os.path.dirname(sys.modules["__main__"].__file__)
+        else:
+            self._save_folder = save_folder
+
+        full_file_path = os.path.join(save_folder, os.path.split(self._url)[1])
+        list_file = os.listdir(self._save_folder)
+        if full_file_path not in list_file:
+            download(self._url, self._save_folder)
+
+        tar = tarfile.open(full_file_path)
+        tar.extractall()
+        tar.close()
 
 if __name__ == '__main__':
-    print("main")
+    CIFAR_10 = cifar_10()
