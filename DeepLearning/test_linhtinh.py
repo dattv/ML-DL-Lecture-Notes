8adfbfc232fa6b54a980c884ca5b516841083c35
dat <datthangvan@gmail.com>
2022-05-28 08:46:32 +0700

    clean branch

--- DeepLearning/test_linhtinh.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import tensorflow as tf
-
-saved_model_dir = "./TFEstimator_cifar10_bin_model/export/Servo/1568993997"
-print(os.path.isdir(saved_model_dir))
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-tflite_model = converter.convert()
-open("converted_model.tflite", "wb").write(tflite_model)
--- TF20/TF20_ex02.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-var = tf.Variable([3, 3])
-if tf.test.is_gpu_available():
-    print("running on GPU")
-    print("GPU: {}".format(var.device.endswith('GPU:0')))
-else:
-    print("Running on CPU")
