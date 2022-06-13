be708a8e5adcc6e245e471177f6e6b440e2ce8a0
dat <datthangvan@gmail.com>
2022-06-13 10:52:50 +0700

    clear all

--- ImageProcessing/l_openCV3.md
+++
@@ -1,2 +0,0 @@
-# Learning Opencv3 with python 
-In here I made a set of example to show how to learn opencv with python 
--- TF20/TF20_ex01.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-print("tensorflow version: {}".format(tf.__version__))
-print("eage excusion is: {}".format(tf.executing_eagerly()))
-print("keras version is: {}".format(tf.keras.__version__))
