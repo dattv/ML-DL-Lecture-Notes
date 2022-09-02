604627453a00a1370a877792af9927c4b29eb524
dattv <datthangvan@gmail.com>
2022-09-02 22:46:51 +0700

    update installation guideline

---
+++ installation_DNN/install_dependencies.sh
@@ -0,0 +1,32 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y build-essential cmake git unzip pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libhdf5-serial-dev graphviz
+sudo apt-get install -y libopenblas-dev libatlas-base-dev gfortran
+sudo apt-get install -y python-tk python3-tk python-imaging-tk
+sudo apt-get install -y python3-dev
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+sudo apt install libgtk-3-dev
+sudo apt install libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev
+sudo apt-get install -y linux-image-generic linux-image-extra-virtual
+sudo apt-get install -y linux-source linux-headers-generic
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libopenblas-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt install libscalapack-mpi-dev
+sudo apt-get install libblas-dev liblapack-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libopenblas-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install openjdk-8-jdk
+
+sudo apt-get install cmake cmake-gui
+sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
---
+++ installation_DNN/opencv.sh
@@ -0,0 +1,29 @@
+# Generic tools:
+sudo apt install build-essential cmake pkg-config unzip yasm git checkinstall
+
+# Image I/O libs
+sudo apt install libjpeg-dev libpng-dev libtiff-dev
+
+# Video/Audio Libs - FFMPEG, GSTREAMER, x264 ans so on
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt install libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev 
+sudo apt install libfaac-dev libmp3lame-dev libvorbis-dev
+
+# OpenCore - Adaptive Multi Rate Narrow Band (AMRNB) and Wide Band (AMRWB) speech codec
+sudo apt install libopencore-amrnb-dev libopencore-amrwb-dev
+
+# Cameras programming interface libs
+sudo apt-get install libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils
+cd /usr/include/linux
+sudo ln -s -f ../libv4l1-videodev.h videodev.h
+cd ~
+
+# GTK lib for the graphical user functionalites coming from OpenCV highghui module
+sudo apt-get install libgtk-3-dev
+sudo apt-get install python3-dev python3-pip
+sudo apt-get install libtbb-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
